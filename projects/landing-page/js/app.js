89636046e1098452206b28ad97aeffc3f652bce0
Hala-001 <hala143375@gmail.com>
2020-10-21 20:32:50 +0300

    make it better

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -2,19 +2,19 @@
 //builds navigation
 function mainFunction(){
     const ul=document.getElementById('navbar__list');
-    var arr = ['section','section','section','section'];
+    var arr = ['section1','section2','section3','section4'];
 // build the nav
 // Build menu 
     for (var i = 0; i < arr.length; i++){
-        var item = document.createElement('il');
+        var item = document.createElement('li');
         var a = document.createElement('a');
-        a.textContent = i;
-        
-        item.textContent = arr[i];
+       
+       item.textContent = arr[i];
         item.style.color= "black";
         item.style.fontSize="30px";
         item.style.margin="30px";
         item.id='navbar'+i;
+        item.className='navbar'+i;
         a.id='navbarr'+i;
         ul.appendChild(item);
         item.appendChild(a);
@@ -32,260 +32,141 @@ const item2 = document.querySelector('#navbar2');
 const item3 = document.querySelector('#navbar3');
 
 
-
 //setAttribut to anchor 1
 const a1 = document.getElementById('navbarr0');
-var att = document.createAttribute("href");
-  att.value = "#section1";
+var att = document.createAttribute("data-page");
+  att.value = "youractiveclass1";
   a1.setAttributeNode(att);
-  a1.style.color='red';
+
 //setAttribut to anchor 2
 const a2 = document.getElementById('navbarr1');
-var att2 = document.createAttribute("href");
-  att2.value = "#section2";
+var att2 = document.createAttribute("data-page");
+  att2.value = "youractiveclass2";
   a2.setAttributeNode(att2);
-  a2.style.color='blue';
+  
 //setAttribut to anchor 3
 const a3 = document.getElementById('navbarr2');
-var att3 = document.createAttribute("href");
-  att3.value = "#section3";
+var att3 = document.createAttribute("data-page");
+  att3.value = "youractiveclass3";
   a3.setAttributeNode(att3);
-  a3.style.color='green';
+  
 //setAttribut to anchor 4
 const a4 = document.getElementById('navbarr3');
 
-var att4 = document.createAttribute("href");
-  att4.value = "#section4";
+var att4 = document.createAttribute("data-page");
+  att4.value = "youractiveclass4";
   a4.setAttributeNode(att4);
-  a4.style.color='Yellow';
- ///////////////////////////////////////
-
-// using scrollTO and give coloer to ul
-window.onscroll = function() {myFunction()};
-function myFunction() {
-  if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
-    const item0 = document.querySelector('#navbar0').style.color='red';
-  } else {
-    const item0 = document.querySelector('#navbar0').style.color='black';
-  }
-  if (document.body.scrollTop > 850 || document.documentElement.scrollTop > 850) {
-    const item1 = document.querySelector('#navbar1').style.color='blue';
-  } else {
-    const item1 = document.querySelector('#navbar1').style.color='black';
-  }
-  if (document.body.scrollTop >1400|| document.documentElement.scrollTop >1400) {
-    const item2 = document.querySelector('#navbar2').style.color='green';
-  } else {
-    const item2 = document.querySelector('#navbar2').style.color='black';
-  }
-  if (document.body.scrollTop >1950|| document.documentElement.scrollTop >1950) {
-    const item3 = document.querySelector('#navbar3').style.color='yellow';
-  } else {
-    const item3 = document.querySelector('#navbar3').style.color='black';
-  }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const mainHeading = document.querySelector('#navbar0');
-
-// mainHeading.addEventListener('click', function () {
-//   mainHeading.style.color='red';
-//   const p1=document.querySelector('.se1');
-
   
-  
+
+// Scroll to section on navigation click
+  const x=document.getElementById("navbar0").addEventListener("click", function() {
+    var element = document.getElementById("section1");
+   
+         
+element.scrollIntoView();
+element.scrollIntoView(false);
+element.scrollIntoView({block: "end"});
+element.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+
+  });
+
+
+
+  // Scroll to section on navigation click
+  const y=document.getElementById("navbar1").addEventListener("click", function() {
+    var element2 = document.getElementById("section2");
+
+element2.scrollIntoView();
+element2.scrollIntoView(false);
+element2.scrollIntoView({block: "end"});
+element2.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+  });
+// Scroll to section on navigation click
+const z=document.getElementById("navbar2").addEventListener("click", function() {
+  var element3 = document.getElementById("section3");
+
+element3.scrollIntoView();
+element3.scrollIntoView(false);
+element3.scrollIntoView({block: "end"});
+element3.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+});
+// Scroll to section on navigation click
+const w=document.getElementById("navbar3").addEventListener("click", function() {
+  var element4 = document.getElementById("section4");
+
+element4.scrollIntoView();
+element4.scrollIntoView(false);
+element4.scrollIntoView({block: "end"});
+element4.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const sections=document.querySelectorAll('section');
+// // console.log(sections);
+// const bubble =document.querySelector('.bubble');
+// // console.log(bubble);
+// const gradients =[
+//         'linear-gradient(to right top, #f46b45,#eea849)',
+//         'linear-gradient(to right top,#005c97,#363795)',
+//         'linear-gradient(to right top,#e53935,#e35d5b)',
+//         'linear-gradient(to right top,#e53935,#e35d5b)'
+//       ];
+// console.log(gradients);
+   
+
+// const options ={
+//   threshold:0.7
+
+// };
+
+
+
+// let observer =new IntersectionObserver(navCheck, options);
+
+// function navCheck(entries){
+// entries.forEach(entry =>{
+//  const className=entry.target.className;
+ 
+//  const activeAnchor=document.querySelector(`[data-page=${className}]`);
+ 
+//  const gradientIndex=entry.target.getAttribute('data-index');
+ 
+//  const coords=activeAnchor.getBoundingClientRect();
+ 
+//  const directions={
+//    height: coords.height,
+//    width: coords.width,
+//    top: coords.top,
+//    left:coords.left
+//  };
+// if(entry.isIntersecting){
+
+
+//   bubble.style.setProperty('left',`${directions.left}px`);
+//   bubble.style.setProperty('top',`${directions.top}px`);
+//   bubble.style.setProperty('width',`${directions.width}px`);
+//   bubble.style.setProperty('height',`${directions.height}px`);
+// }
+
 // });
+// }
 
+// sections.forEach(section =>{
 
+//   observer.observe(section);
+// } )
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var target = document.querySelector('#section4');
-
-
-
-// const options = {
-//     root: null,
-//     rootMargin: '0px',
-//     threshold: 1.0
-//   };
-
-  
-//   let callback= (entries, observer)=> {
-//     entries.forEach(entry => {
-//       if(entry.target.id===target){
-//         item3.style.color='red';}
-//         else{
-//         item3.style.color='Yellow';
-       
-//       }
-//       console.log(entry);
-//     });
-//   }
-  
-//   let observer = new IntersectionObserver(callback, options);
-//   observer.observe(target);
-//   console.log(target);
-
-
-  ///////////////////////////////////////
-  //                                   //
-  //////////////////////////////////////
-
-// var target3 = document.querySelector('#section3');
-// let callback= (entries, observer)=> {
-//   //observer.observe(target);
-//   entries.forEach(entry => {
-//     console.log("entry.target.id "+entry.target.id)
-//     console.log("target "+target.id);
-//     if(entry.target.id==target.id){
-//       console.log(item3);
-//       item3.style.color='red';
-//     }if(entry.target3.id==target3.id){
-//       item3.style.color='Yellow';
-//       item3.style.color='red';
-//     }
-//     console.log(entry);
-//     });
-//   }
-
-
-// let callback= (entries, observer)=> {
-//   entries.forEach(entry => {
-//     //console.log("entry.target.id "+entry.target.id)
-//     //console.log("target "+target.id);
-//     if(entry.target3.id==target3.id){
-//       item3.style.color='Yellow';
-//       item3.style.color='red';
-//       }console.log(entry);});
-//       }
-  
-// let observer = new IntersectionObserver(callback, options);
-// observer.observe(target);
-
-// let observer3 = new IntersectionObserver(callback, options);
-// observer3.observe(target3);
-
-  ///////////////////////////////////////
-  //                                   //
-  //////////////////////////////////////
-  
-//style for elemint section
-// const style0=item0.style.color='red';
-// const style1=item1.style.color='red';
-// const style2=item2.style.color='red';
-// const style3=item3.style.color='red';
-
-
-
-
-
-
-
-
-
-
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
-
-/**
- * Define Global Variables
- * 
-*/
-
-
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-
-
-// Scroll to section on link click
-
-// Set sections as active
 
 
